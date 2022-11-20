d79f3777cf5809d0882b4f5f59f689f2d8ddbbae
YokoyamaGen <g.yokoyama.0616@gmail.com>
2022-11-20 17:20:04 +0900

    フィルター機能を追加

--- js/todo.js
+++ js/todo.js
@@ -1,17 +1,37 @@
 new Vue({
   el: "#app",
   data: {
-    todoList: [],
+    todoList: [
+      "Todo1",
+      "Todo2",
+    ],
+    keyword: "",
   },
   methods: {
     additem: function (arg) {
       this.todoList.push(arg);
       this.$refs.todoText.value = "";
     },
-    deleteitem: function (index) {
-      if (confirm("「" + this.todoList[index] + "」のtodoを削除しますか？")) {
-        this.todoList.splice(index, 1);
+    deleteitem: function (todo) {
+      let deleteIndex = this.todoList.indexOf(todo);
+      if (
+        confirm(
+          "「" + this.todoList[deleteIndex] + "」のtodoを削除しますか？"
+        )
+      ) {
+        this.todoList.splice(deleteIndex, 1);
       }
+    },
+  },
+  computed: {
+    filteredTodo: function () {
+      let todoList = [];
+      for (let todo of this.todoList) {
+        if (todo.indexOf(this.keyword) !== -1) {
+          todoList.push(todo);
+        }
+      }
+      return todoList;
     },
   },
 });
